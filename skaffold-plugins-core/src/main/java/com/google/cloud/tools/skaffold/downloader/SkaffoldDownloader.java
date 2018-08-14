55205fa27b0be0833c7550849b8907a46f38681b
Qingyang Chen <qingyangc@google.com>
2018-08-14 13:37:20 -0400

    feedback

--- skaffold-plugins-core/src/main/java/com/google/cloud/tools/skaffold/downloader/SkaffoldDownloader.java
+++ skaffold-plugins-core/src/main/java/com/google/cloud/tools/skaffold/downloader/SkaffoldDownloader.java
@@ -16,6 +16,10 @@
 
 package com.google.cloud.tools.skaffold.downloader;
 
+import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+
 import com.google.cloud.tools.skaffold.filesystem.OperatingSystem;
 import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
@@ -24,10 +28,15 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.HashSet;
+import java.util.EnumSet;
 import java.util.Set;
 
-/** Downloads {@code skaffold} executable. */
+/**
+ * Downloads {@code skaffold} executable.
+ *
+ * @see <a
+ *     href="https://github.com/GoogleContainerTools/skaffold/blob/master/docs/published-artifacts.adoc">https://github.com/GoogleContainerTools/skaffold/blob/master/docs/published-artifacts.adoc</a>
+ */
 public class SkaffoldDownloader {
 
   /**
@@ -37,6 +46,7 @@ public class SkaffoldDownloader {
    * @throws MalformedURLException if the URL to download from is malformed
    */
   public static void downloadLatest(Path destination) throws IOException {
+    // Skaffold publishes the latest release with version "latest".
     download(new URL(getUrl("latest", OperatingSystem.resolve())), destination);
   }
 
@@ -53,14 +63,14 @@ public class SkaffoldDownloader {
     }
 
     // Makes skaffold executable.
-    OperatingSystem os = OperatingSystem.resolve();
-    if (os == OperatingSystem.LINUX || os == OperatingSystem.MAC_OS) {
+    try {
       Set<PosixFilePermission> executableFilePermissions =
-          new HashSet<>(Files.getPosixFilePermissions(destination));
-      executableFilePermissions.add(PosixFilePermission.OWNER_EXECUTE);
-      executableFilePermissions.add(PosixFilePermission.GROUP_EXECUTE);
-      executableFilePermissions.add(PosixFilePermission.OTHERS_EXECUTE);
+          Files.getPosixFilePermissions(destination);
+      executableFilePermissions.addAll(EnumSet.of(OWNER_EXECUTE, GROUP_EXECUTE, OTHERS_EXECUTE));
       Files.setPosixFilePermissions(destination, executableFilePermissions);
+
+    } catch (UnsupportedOperationException ex) {
+      // File system does not support POSIX.
     }
   }
 
