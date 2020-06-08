80229eb3545dbb334ef4e2ecea8f6add8a7b9181
Brian de Alwis <bdealwis@google.com>
2020-06-08 17:29:19 -0400

    use bitnami/kubectl image as it is much smaller

--- run-its.sh
+++ run-its.sh
@@ -12,7 +12,9 @@ skaffoldPid=$!
 trap "echo '>> Tearing down test jobs [$(date)]'; kill $skaffoldPid; skaffold delete -p integration" 0 1 3 15
 
 echo ">> Waiting for test jobs to start [$(date)]"
-sleep 10
+while [ $(kubectl get job.batch -o name | wc -l) -eq 0 ]; do
+    sleep 5
+done
 echo ">> Monitoring for test job completion [$(date)]"
 kubectl wait --for=condition=complete job.batch \
     -l project=container-debug-support,type=integration-test
