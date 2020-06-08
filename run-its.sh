a10ac7af83f029ad436f246d0860fc355bcf9cd5
Brian de Alwis <bdealwis@google.com>
2020-06-08 15:45:05 -0400

    tweak and tweak

--- run-its.sh
+++ run-its.sh
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
-echo ">> Building..."
+echo ">> Building test images [$(date)]"
 skaffold build -p integration --skip-tests
 
-echo ">> Launching test jobs and pods"
+echo ">> Launching test jobs and pods [$(date)]"
 skaffold run -p integration --tail &
 skaffoldPid=$!
 
-trap "echo '>> Tearing down test jobs'; kill $skaffoldPid; skaffold delete -p integration" 0 1 3 15
+trap "echo '>> Tearing down test jobs [$(date)]'; kill $skaffoldPid; skaffold delete -p integration" 0 1 3 15
 
-echo ">> Waiting for test jobs to start"
-sleep 5
-echo ">> Monitoring for test job completion"
+echo ">> Waiting for test jobs to start [$(date)]"
+sleep 10
+echo ">> Monitoring for test job completion [$(date)]"
 kubectl wait --for=condition=complete job.batch \
     -l project=container-debug-support,type=integration-test
