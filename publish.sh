700d77542a4eef5d4025255b5f0cb78d51658e10
Brian de Alwis <bdealwis@google.com>
2021-02-08 09:16:44 -0500

    Publish images with more descriptive names
    Github's Packages and GHCR.io offer relatively flat namespaces which
    means images like `go`, `python`, or `nodejs` can cause confusion.
    This patch renames the images to variants of `skaffold-debug-xxx`,
    but continues to publish the images to the original locations
    (gcp-dev-tools, k8s-skaffold) using the shortened name.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+set -eu
+
+# publish with longer image names
 skaffold build -p prod --default-repo gcr.io/k8s-skaffold/skaffold-debug-support
 skaffold build -p prod --default-repo gcr.io/gcp-dev-tools/duct-tape
+
+# the github project packages is a backup location; will need to
+# migrate to ghcr.io/googlecontainertools at some point
+skaffold build -p prod --default-repo docker.pkg.github.com/googlecontainertools/skaffold
+
+# publish with shorter (deprecated) image names
+skaffold build -p prod,deprecated-names --default-repo gcr.io/k8s-skaffold/skaffold-debug-support
+skaffold build -p prod,deprecated-names --default-repo gcr.io/gcp-dev-tools/duct-tape
