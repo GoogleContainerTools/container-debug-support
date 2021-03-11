73053961c982bf9962284fcbc69dc33c75b7077a
Brian de Alwis <bdealwis@google.com>
2021-03-11 14:14:38 -0500

    do not copy and paste from the internet

--- hack/enable-docker-buildkit.sh
+++ hack/enable-docker-buildkit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Buildkit requires Docker to have experimental features enabled <https://stackoverflow.com/a/57849395>
 if [ -f /etc/docker/daemon.json ]; then
-    cat <<< $(jq '.+{"experimental":"enabled"}' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json
+    jq '.+{"experimental":"enabled"}' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json
 else
     echo '{"experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
 fi
