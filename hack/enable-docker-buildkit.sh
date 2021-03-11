622aab7b1059a1187334237d5f1c27655d4a3104
Brian de Alwis <bdealwis@google.com>
2021-03-11 15:34:57 -0500

    move buildkit enablement into before_install

--- hack/enable-docker-buildkit.sh
+++ hack/enable-docker-buildkit.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # Buildkit requires Docker to have experimental features enabled <https://stackoverflow.com/a/57849395>
+
 if [ -f /etc/docker/daemon.json ]; then
+    echo "/etc/docker/daemon.json was:"
+    sed 's/^/> /etc/docker/daemon.json
+    echo "/etc/docker/daemon.json now:"
     jq '.+{"experimental":"enabled"}' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json
 else
+    echo "/etc/docker/daemon.json now:"
     echo '{"experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
 fi
+sudo chown travis:travis /etc/docker/daemon.json
 sudo service docker restart
