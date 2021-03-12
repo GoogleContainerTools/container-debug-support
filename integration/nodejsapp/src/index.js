16ce8dda13638c43153189ca743d459e1cb401f4
Brian de Alwis <bdealwis@google.com>
2021-03-12 15:51:47 -0500

    Support linux/arm64 and linux/amd64 (#64)
    * Use `docker buildx` to build for linux/arm64 and linux/amd64
       * Update Travis to use bionic and use newer docker
    * Update dotnet builder image to dotnet/runtime:6.0 for linux/arm64 (no difference in 
    * Add test for Go 1.16

--- integration/nodejsapp/src/index.js
+++ integration/nodejsapp/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { echo } = require('./utils');
+const os = require('os');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(echo('Hello World!')))
+app.get('/', (req, res) => res.send(echo(`Hello from ${os.platform()}/${os.arch()}!\n`)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
