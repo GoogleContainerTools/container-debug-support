ebacb8202abc1d92db61e1a0130e33e0c7602faa
Brian de Alwis <bdealwis@google.com>
2021-03-11 22:22:47 -0500

    include forgotten go116 and publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -eu
 
+# note: we build the first build with --cache-artifacts=false to
+# avoid cache poisoning from single-platform builds
+# https://github.com/GoogleContainerTools/skaffold/issues/5504
+
 # publish with longer image names
-skaffold build -p prod --default-repo gcr.io/k8s-skaffold/skaffold-debug-support
+skaffold build -p prod --default-repo gcr.io/k8s-skaffold/skaffold-debug-support --cache-artifacts=false
 skaffold build -p prod --default-repo gcr.io/gcp-dev-tools/duct-tape
 
 # the github project packages is a backup location; will need to
