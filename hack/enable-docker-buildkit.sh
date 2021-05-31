f53c5860cb3392be8908df6146508fbbaf5a365e
Brian de Alwis <bdealwis@google.com>
2021-05-31 15:34:19 -0400

    use travis `apt:` to install updated docker-ce

--- hack/enable-docker-buildkit.sh
+++ hack/enable-docker-buildkit.sh
@@ -7,13 +7,13 @@ if [ -f /etc/docker/daemon.json ]; then
     sed 's/^/> /' /etc/docker/daemon.json
     echo "/etc/docker/daemon.json now:"
     jq '.+{"experimental":true}' /etc/docker/daemon.json \
+    | jq '."registry-mirrors" -= ["https://registry.docker.io"]' \
     | jq '."registry-mirrors" += ["https://mirror.gcr.io"]' \
     | sudo tee /etc/docker/daemon.json
 else
     sudo mkdir -vp /etc/docker
     echo "/etc/docker/daemon.json now:"
-    echo '{"experimental":true}' \
-    | jq '."registry-mirrors" += ["https://mirror.gcr.io"]' \
+    echo '{"experimental":true,"registry-mirrors":["https://mirror.gcr.io"]}' \
     | sudo tee /etc/docker/daemon.json
 fi
 
@@ -21,20 +21,16 @@ if [ -f $HOME/.docker/config.json ]; then
     echo "$HOME/.docker/config.json was:"
     sed 's/^/> /' $HOME/.docker/config.json
     echo "$HOME/.docker/config.json now:"
-    jq '.+{"experimental":"enabled"}' /etc/docker/daemon.json | tee $HOME/.docker/config.json
+    jq '.+{"experimental":"enabled"}' /etc/docker/daemon.json \
+    | tee $HOME/.docker/config.json
 else 
     mkdir -vp $HOME/.docker
     echo "$HOME/.docker/config.json now:"
-    echo '{"experimental":"enabled"}' | tee $HOME/.docker/config.json
+    echo '{"experimental":"enabled"}' \
+    | tee $HOME/.docker/config.json
 fi
-
-echo ">> updating docker engine"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 
 echo ">> installing docker-buildx"
 mkdir -vp $HOME/.docker/cli-plugins/
-curl --silent -L "https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-amd64" > $HOME/.docker/cli-plugins/docker-buildx
+curl --silent -L "https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-${TRAVIS_CPU_ARCH}" > $HOME/.docker/cli-plugins/docker-buildx
 chmod a+x $HOME/.docker/cli-plugins/docker-buildx
