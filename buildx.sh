400349a346a47404b9beae4acd6584b1775d6e2e
Brian de Alwis <bdealwis@google.com>
2021-05-31 15:17:38 -0400

    install jq and just use plain docker if not pushing

--- buildx.sh
+++ buildx.sh
@@ -3,22 +3,27 @@
 # Skaffold Custom Builder that uses `docker buildx` to perform a
 # multi-platform build.
 
+if [ "$PUSH_IMAGE" != true ]; then
+    export DOCKER_BUILDKIT=1
+    exec docker build --tag $IMAGE "$BUILD_CONTEXT"
+fi
+
 PLATFORMS=linux/amd64,linux/arm64
 
 if ! docker buildx inspect skaffold-builder >/dev/null 2>&1; then
-  echo ">> creating "docker buildx" builder 'skaffold-builder'"
+  echo ">> creating 'docker buildx' builder 'skaffold-builder'"
   # Docker 3.3.0 require creating a builder within a context
-  docker context create skaffold
-  docker buildx create --name skaffold-builder --platform $PLATFORMS skaffold
+  (set -x; \
+    docker context create skaffold; \
+    docker buildx create --name skaffold-builder --platform $PLATFORMS skaffold)
 fi
-
-loadOrPush=$(if [ "$PUSH_IMAGE" = true ]; then echo --platform $PLATFORMS --push; else echo --load; fi)
 
 set -x
 docker buildx build \
   --progress=plain \
   --builder skaffold-builder \
-  $loadOrPush \
+  --platform "$PLATFORMS" \
+  --push \
   --tag $IMAGE \
   "$BUILD_CONTEXT"
 
