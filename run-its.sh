be7453f7f970232e5cbecb2ad29723bee61385ca
Brian de Alwis <bdealwis@google.com>
2021-06-23 10:34:13 -0400

    pass `-p local` into run-its.sh

--- run-its.sh
+++ run-its.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+# run integration tests
+# any arguments are passed onto skaffold
+#
+# $ sh run-its.sh -p local
+#
 
 set -euo pipefail
 
 echo ">> Building test images [$(date)]"
-skaffold build -p integration
+skaffold build -p integration "$@"
 
 echo ">> Launching test jobs and pods [$(date)]"
-skaffold run -p integration --tail &
+skaffold run -p integration --tail "$@" &
 skaffoldPid=$!
 
 trap "echo '>> Tearing down test jobs [$(date)]'; kill $skaffoldPid; skaffold delete -p integration" 0 1 3 15
 
 echo ">> Waiting for test jobs to start [$(date)]"
-# at least 5 tests = go 1.13 1.14 1.15 1.16 + nodejs 12
-while [ $(kubectl get job.batch -o name | wc -l) -lt 5 ]; do
+while [ $(kubectl get job.batch -o name -l project=container-debug-support,type=integration-test | wc -l) -lt 1 ]; do
     sleep 5
 done
+# Do an additional wait to ensure all tests are up and running as
+# `kubectl wait` doesn't refresh and will miss late appearing resources
+sleep 5
+kubectl get job.batch -o name -l project=container-debug-support,type=integration-test
+
 echo ">> Monitoring for test job completion [$(date)]"
 kubectl wait --for=condition=complete job.batch \
     -l project=container-debug-support,type=integration-test
