3671e335180a55a85475835246d1e5f5032218f6
Brian de Alwis <bdealwis@google.com>
2021-06-25 11:44:30 -0400

    rejig builds to use `docker build` for local platform

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 #
 # Skaffold Custom Builder that uses `docker buildx` to perform a
-# multi-platform build.
+# multi-platform build when necessary.
+
+export DOCKER_BUILDKIT=1
 
 if [ "$PUSH_IMAGE" = true ]; then
     if [ -z "$PLATFORMS" ]; then
         PLATFORMS=linux/amd64,linux/arm64
     fi
     loadOrPush="--platform $PLATFORMS --push"
+elif [ "$USE_BUILDX" != true ]; then
+    # cannot load multiarch images into the daemon
+    # so just use `docker build`
+    set -x
+    exec docker build "$@" --tag $IMAGE "$BUILD_CONTEXT"
 else
     # cannot load multiarch images into the daemon
     unset PLATFORMS
@@ -27,6 +34,7 @@ docker buildx build \
   --progress=plain \
   --builder skaffold-builder \
   $loadOrPush \
+  "$@" \
   --tag $IMAGE \
   "$BUILD_CONTEXT"
 
